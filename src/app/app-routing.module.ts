a5e7487986a4f4524ff16efeeb9a6357ffab1108
nataliabalakhashvili <natalia.balakhashvili@geolab.edu.ge>
2023-02-27 19:58:18 +0400

    feat - header styling

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,18 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-
+  {
+    path: 'home',
+    loadChildren: () =>
+      import('./features/home/home.module').then((m) => m.HomeModule)
+  },
+  // {
+  //   path: 'not-found',
+  //   loadChildren: () =>
+  //     import('./features/page-not-found/page-not-found.module').then((m) => m.PageNotFoundModule)
+  // },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '**', redirectTo: 'home'}
 ];
 
 @NgModule({
--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { MenuItem } from "./entity/header.interface";
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent {
+  menuItems: MenuItem[] = [
+    {
+      title: 'home',
+      url: '/home'
+    },
+    {
+      title: 'about us',
+      url: '/about'
+    },
+    {
+      title: 'blog entries',
+      url: '/blogs'
+    },
+    {
+      title: 'contact us',
+      url: '/contact'
+    }
+  ]
 
   constructor() { }
-
-  ngOnInit(): void {
-  }
 
 }
