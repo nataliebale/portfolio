9ca9fc230a52ca5d90cb5713a73b360633cb5034
nataliabalakhashvili <natalia.balakhashvili@geolab.edu.ge>
2023-02-27 18:02:59 +0400

    feat - architecture (styles arch 7-11, fonts, routing)

---
+++ src/app/features/home/home.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HomeComponent } from './home.component';
+import { RouterModule, Routes } from "@angular/router";
+
+const ROUTES: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  }
+]
+
+@NgModule({
+  declarations: [
+    HomeComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(ROUTES),
+  ]
+})
+export class HomeModule { }
